7955b43385f43d1361f7342a7fba98db270300bc
Onur Cankaya <oecankaya@gmail.com>
2016-11-16 17:24:47 +0100

    Clean up syntax with ES6

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
